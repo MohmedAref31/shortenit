e37dab4d9a634cfaa28d77764a375da6fda80a4b
mohmedaref31 <mohmedaref2003@gmail.com>
2026-02-12 21:49:24 +0200

    feat: add internationalization support and authentication module to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { I18nModule, QueryResolver, AcceptLanguageResolver } from 'nestjs-i18n';
+import path from 'path';
 import { UrlModule } from './modules/url/url.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env.dev', '.env.prod'],
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      typesOutputPath: path.join(
+        __dirname,
+        '../src/generated/i18n.generated.ts',
+      ),
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
@@ -21,6 +39,7 @@ import { UserModule } from './modules/user/user.module';
     }),
     UrlModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
